8914f1cdc4c7313a5ae342f5e45095b9cb866934
HaianhNguyenHo <nguyenhohaianh2@gmail.com>
2021-09-24 22:19:23 +0700

    sửa đổi mã nguồn tuần 1

--- src/vn/edu/giadinh/Program.java
+++ src/vn/edu/giadinh/Program.java
@@ -1,9 +1,8 @@
-package vn;
+package vn.edu.giadinh;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello Program");
-        
+        System.out.println("hello Pỏgram!!");
     }
     
 }
