df082f41e14c7569b0d55471c77255c67ed9e031
HaianhNguyenHo <nguyenhohaianh2@gmail.com>
2021-09-24 22:07:11 +0700

    Sửa lại mã nguồn tuần 1

--- src/vn/Program.java
+++ src/vn/Program.java
@@ -2,7 +2,8 @@ package vn;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello Program!!");
+        System.out.println("Hello Program");
+        
     }
     
 }
