fef5a16f790da0aee453976907b7c3e5b6cfa90e
HaianhNguyenHo <nguyenhohaianh2@gmail.com>
2021-11-10 08:42:15 +0700

    donate source code ktgk cau 1

---
+++ src/vn/edu/giadinh/suabaikiemtragiuaky/Apple.java
@@ -0,0 +1,22 @@
+package vn.edu.giadinh.suabaikiemtragiuaky;
+
+public class Apple {
+    int id;
+    String color;
+    double weight;
+
+    public Apple(){}
+
+    public Apple(int _id, String _color, double _weight){
+        id=_id;
+        color = _color;
+        weight= _weight;
+    }
+    //nhập
+
+    //in thông tin
+    public void printApple(){
+        
+    }
+    
+}
---
+++ src/vn/edu/giadinh/suabaikiemtragiuaky/AppleInventoryTestDrive.java
@@ -0,0 +1,8 @@
+package vn.edu.giadinh.suabaikiemtragiuaky;
+
+public class AppleInventoryTestDrive {
+    public static void main(String[] args) {
+        
+    }
+    
+}
